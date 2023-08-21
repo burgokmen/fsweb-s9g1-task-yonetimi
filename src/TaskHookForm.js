d94924e536b15fff2a956321beb585553dfa993f
burgokmen <burgokmen@gmail.com>
2023-08-21 20:50:43 +0300

    handle func done,form ongoing

--- src/TaskHookForm.js
+++ src/TaskHookForm.js
@@ -1,7 +1,18 @@
-import React from 'react'
+import React from "react";
+import { useForm } from "react-hook-form";
 
-export default function TaskHookForm() {
-  return (
-    <div>Formunuzu react-hook-form kullanarak burada oluşturun. TaskForm dosyasındaki HTML yapısını vs app.css içerisindeki classları kullanabilirsiniz.</div>
-  )
-}
+const TaskHookForm = ({ kisiler, submitFn }) => {
+  const { register, handleSubmit } = useForm({
+    defaultValues: {
+      title: "",
+      description: "",
+      people: [],
+    },
+  });
+
+  return <form onSubmit={handleSubmit(submitFn)}></form>;
+};
+export default TaskHookForm;
+
+/*     <div>Formunuzu react-hook-form kullanarak burada oluşturun. 
+    TaskForm dosyasındaki HTML yapısını vs app.css içerisindeki classları kullanabilirsiniz.</div> */
